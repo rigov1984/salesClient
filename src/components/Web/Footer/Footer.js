e2a149e32dff611b7dee0e7801f882f47f924162
rigov1984 <rigov18@hotmail.com>
2021-03-12 10:53:59 -0500

    Se adiciona la funcion para consumir el enpoint de newsletter

--- src/components/Web/Footer/Footer.js
+++ src/components/Web/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Layout, Row, Col } from "antd";
 import MyInfo from "./MyInfo";
 import NavigationFooter from "./NavigationFooter";
+import Newsletter from "../Newsletter";
 
 import "./Footer.scss";
 
@@ -20,7 +21,9 @@ export default function Footer() {
                         <Col md={8}>
                             <NavigationFooter />
                         </Col>
-                        <Col md={8}>News leter</Col>
+                        <Col md={8}>
+                            <Newsletter />
+                        </Col>
                     </Row>
                     <Row className="footer__copyright">
                         <Col md={12}>© TODOS LOS DERECHOS RESERVADOS</Col>
